9e125a3eff78084ce92b9ba74f65ecd637ceec86
steve.campos <steve.campos@rakuten.com>
2022-11-19 21:53:36 +0100

    revert config

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function App() {
     return (
         <div>
-            <h1>Plug my life</h1>
+            <h1>Host Application!</h1>
         </div>
     );
 }
