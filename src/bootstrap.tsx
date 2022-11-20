d25dd15276273c29b58b27d999838bc658ba6845
steve.campos <steve.campos@rakuten.com>
2022-11-20 22:55:12 +0100

    update webpack config

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -4,4 +4,8 @@ import App from './App';
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -25,24 +25,9 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js'],
     },
     plugins: [
-        new ModuleFederationPlugin({
-            name: 'pluggy-host',
-            shared: {
-                react: {
-                    requiredVersion: deps.react,
-                    import: 'react', // the "react" package will be used a provided and fallback module
-                    shareKey: 'react', // under this name the shared module will be placed in the share scope
-                    shareScope: 'default', // share scope with this name will be used
-                    singleton: true, // only a single version of the shared module is allowed
-                },
-                'react-dom': {
-                    requiredVersion: deps['react-dom'],
-                    singleton: true, // only a single version of the shared module is allowed
-                },
-            },
-        }),
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'public', 'index.html'),
+            inject: false,
         }),
     ],
 };
